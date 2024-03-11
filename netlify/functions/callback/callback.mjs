27e881e802e0a49c26e05f85cba05a257925d0d5
Jordan McNamara <jordan.mcnamara@gmail.com>
2024-03-11 23:48:47 +0000

    adding get_token fetch

--- netlify/functions/callback/callback.mjs
+++ netlify/functions/callback/callback.mjs
@@ -5,6 +5,7 @@ export const handler = async (event, context) => {
       client_id: process.env.CLIENT_ID,
       client_secret: process.env.CLIENT_SECRET,
       redirect_uri: process.env.REDIRECT_URI,
+      grant_type: "authorization_code",
       code,
     };
     const response = await fetch(

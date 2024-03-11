518742a20eb6e22341c3247bf932802d7ae7b221
Jordan McNamara <jordan.mcnamara@gmail.com>
2024-03-11 23:41:42 +0000

    adding get_token fetch

--- netlify/functions/callback/callback.mjs
+++ netlify/functions/callback/callback.mjs
@@ -1,10 +1,23 @@
 export const handler = async (event, context) => {
-  console.log(event.queryStringParameters);
-
-  return {
-    body: JSON.stringify({
-      message: `Hello World`,
-    }),
-    statusCode: 200,
-  };
+  const { code } = event.queryStringParameters;
+  if (code) {
+    const payload = {
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      redirect_uri: process.env.REDIRECT_URI,
+      code,
+    };
+    const response = await fetch(
+      "https://api.login.yahoo.com/oauth2/get_token",
+      { method: "POST", body: JSON.stringify(payload) }
+    );
+    const data = await response.json();
+    return {
+      body: JSON.stringify({
+        message: `Hello World`,
+        data,
+      }),
+      statusCode: 200,
+    };
+  }
 };
--- netlify/functions/request-auth/request-auth.mjs
+++ netlify/functions/request-auth/request-auth.mjs
@@ -3,6 +3,7 @@ export const handler = async (event, context) => {
   const baseURL = "https://api.login.yahoo.com/oauth2/request_auth";
   const clientID = `client_id=${CLIENT_ID}`;
   const redirectURI = `redirect_uri=${REDIRECT_URI}`;
+  console.log(`${baseURL}?${clientID}&${redirectURI}&response_type=code`);
   return {
     headers: {
       Location: `${baseURL}?${clientID}&${redirectURI}&response_type=code`,

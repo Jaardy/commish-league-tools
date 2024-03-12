276851390790635204933a51d2ec2c608f231f3b
Jordan McNamara <jordan.mcnamara@gmail.com>
2024-03-12 00:10:42 +0000

    adding get_token fetch

--- netlify/functions/callback/callback.mjs
+++ netlify/functions/callback/callback.mjs
@@ -1,5 +1,6 @@
 export const handler = async (event, context) => {
   const { code } = event.queryStringParameters;
+  console.log(event);
   if (code) {
     const baseURL = "https://api.login.yahoo.com/oauth2/get_token";
     const client_id = `client_id=${process.env.CLIENT_ID}`;
@@ -7,7 +8,6 @@ export const handler = async (event, context) => {
     const redirect_uri = `redirect_uri=${process.env.REDIRECT_URI}`;
     const grant_type = "grant_type=authorization_code";
     const query = `${client_id}&${client_secret}&${redirect_uri}&${grant_type}&code=${code}`;
-    console.log(`${baseURL}?${query}`);
     const response = await fetch(`${baseURL}?${query}`, { method: "POST" });
     const data = await response.json();
     return {
